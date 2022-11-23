a3a1733c1114c8b024a2e5cdd5a13f5f58e3c8aa
Milorann <57359954+Milorann@users.noreply.github.com>
2022-11-23 20:00:57 +0300

    Update main.s

--- Assembly_modified/main.s
+++ Assembly_modified/main.s
@@ -20,9 +20,9 @@ main:
 	
 	cmp	DWORD PTR -68[rbp], 5       # argc сранивается с 5
 	je	.L2
-	lea	rdi, .LC0[rip]
+	lea	rdi, .LC0[rip]              # аргумент для printf (puts)
 	call	puts@PLT
-	mov	edi, 1
+	mov	edi, 1                      # аргумент для exit
 	call	exit@PLT
 	
 .L2:
@@ -49,57 +49,58 @@ main:
 	mov	eax, 0
 	call	generate@PLT
 	movq	QWORD PTR -24[rbp], xmm0    # x = generate()
+
 .L4:
-	movsd	xmm0, QWORD PTR .LC2[rip]
-	comisd	xmm0, QWORD PTR -24[rbp]
+	movsd	xmm0, QWORD PTR .LC2[rip]   # -M_PI_2
+	comisd	xmm0, QWORD PTR -24[rbp]    # сравнение -M_PI_2 с x
 	jnb	.L5
-	movsd	xmm0, QWORD PTR -24[rbp]
-	comisd	xmm0, QWORD PTR .LC3[rip]
+	movsd	xmm0, QWORD PTR -24[rbp]    # x
+	comisd	xmm0, QWORD PTR .LC3[rip]   # сравнение x с M_PI_2
 	jb	.L11
+	
 .L5:
-	lea	rax, .LC4[rip]
-	mov	rdi, rax
+	lea	rdi, .LC4[rip]                  # аргумент для printf (puts)
 	call	puts@PLT
-	mov	edi, 1
+	mov	edi, 1                          # аргумент для exit
 	call	exit@PLT
+	
 .L11:
 	pxor	xmm0, xmm0
-	movsd	QWORD PTR -32[rbp], xmm0
-	mov	edi, 0
+	movsd	QWORD PTR -32[rbp], xmm0    # ans = 0
+	mov	edi, 0                          # аргумент для time
 	call	time@PLT
-	mov	QWORD PTR -48[rbp], rax
-	mov	DWORD PTR -36[rbp], 0
+	mov	QWORD PTR -48[rbp], rax         # time_t start = time(NULL)
+	mov	DWORD PTR -36[rbp], 0           # i = 0
 	jmp	.L8
+	
 .L9:
-	mov	rax, QWORD PTR -24[rbp]
-	movq	xmm0, rax
+	movq	xmm0, QWORD PTR -24[rbp]    # аргумент для tg
 	call	tg@PLT
-	movq	rax, xmm0
-	mov	QWORD PTR -32[rbp], rax
-	add	DWORD PTR -36[rbp], 1
+	movq	QWORD PTR -32[rbp], xmm0    # ans = tg(x)
+	add	DWORD PTR -36[rbp], 1           # i++
+	
 .L8:
-	mov	rax, QWORD PTR -80[rbp]
-	add	rax, 32
-	mov	rax, QWORD PTR [rax]
-	mov	rdi, rax
+	mov	rax, QWORD PTR -80[rbp]         # argv
+	add	rax, 32                         # argv[4]
+	mov	rdi, QWORD PTR [rax]            # аргумент для atoi (argv[4])
 	call	atoi@PLT
-	cmp	DWORD PTR -36[rbp], eax
+	cmp	DWORD PTR -36[rbp], eax         # сравнение i с atoi(argv[4])
 	jl	.L9
-	mov	edi, 0
+	mov	edi, 0                          # аргумент для time
 	call	time@PLT
-	mov	QWORD PTR -56[rbp], rax
-	mov	rax, QWORD PTR -56[rbp]
-	sub	rax, QWORD PTR -48[rbp]
-	mov	rbx, rax
-	mov	rax, QWORD PTR -24[rbp]
-	movq	xmm0, rax
+	mov	QWORD PTR -56[rbp], rax         # time_t end = time(NULL)
+	
+	mov	rbx, QWORD PTR -56[rbp]         # end
+	sub	rbx, QWORD PTR -48[rbp]         # (end - start)
+	mov	rax, QWORD PTR -24[rbp]         # x
+	movq	xmm0, rax                   # аргумент для tan (x)
 	call	tan@PLT
-	movq	rcx, xmm0
-	mov	rax, QWORD PTR -80[rbp]
-	add	rax, 16
-	mov	rax, QWORD PTR [rax]
-	movsd	xmm0, QWORD PTR -32[rbp]
-	mov	rdx, QWORD PTR -24[rbp]
+	movq	rcx, xmm0                   # tan(x)
+	mov	rax, QWORD PTR -80[rbp]         # argv
+	add	rax, 16                         # argv[2]
+	mov	rax, QWORD PTR [rax]            # argv[2]
+	movsd	xmm0, QWORD PTR -32[rbp]    # ans
+	mov	rdx, QWORD PTR -24[rbp]         # x
 	mov	rsi, rbx
 	movq	xmm2, rcx
 	movapd	xmm1, xmm0
@@ -111,12 +112,14 @@ main:
 	leave
 	ret
 	.size	main, .-main
+	
 	.section	.rodata
 	.align 8
-.LC2:
+.LC2:                           # -M_PI_2
 	.long	1413754136
 	.long	-1074191877
+	
 	.align 8
-.LC3:
+.LC3:                           # M_PI_2
 	.long	1413754136
 	.long	1073291771
